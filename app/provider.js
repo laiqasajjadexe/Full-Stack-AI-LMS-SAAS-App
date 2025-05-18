f0296e65fa0744ca6307a6e8a729cf0c4671c514
laiqasajjadexe <laiqasajjad56@gmail.com>
2025-05-18 17:43:02 +0500

    chore: add Provider component for user management and API integration

---
+++ app/provider.js
@@ -0,0 +1,37 @@
+"use client"
+import { useUser } from '@clerk/nextjs'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+
+
+function Provider({ children }) {
+    const { user } = useUser();
+    useEffect(() => {
+        user&&CheckIsNewUser();
+    }, [user]);
+
+    const CheckIsNewUser = async () => {
+        // const result = await db.select()
+        //     .from(USER_TABLE)
+        //     .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
+        // console.log(result);
+        // if (result?.length == 0) {
+        //     const userResp = await db.insert(USER_TABLE)
+        //         .values({
+        //             name: user?.fullName,
+        //             email: user?.primaryEmailAddress?.emailAddress,
+        //         })
+        //         .returning({ id: USER_TABLE.id });
+        //     console.log(userResp);
+        // }
+        const res = await axios.post('/api/create-user', {user:user});
+        console.log(res.data);
+    }
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default Provider
