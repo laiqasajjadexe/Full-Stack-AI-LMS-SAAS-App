c2169fce469f29cae8ee0e6e8e717af6510fe7a6
laiqasajjadexe <laiqasajjad56@gmail.com>
2025-05-19 23:45:19 +0500

    fix: correct typo in className attribute in WelcomeBanner component

--- app/dashboard/_components/WelcomeBanner.jsx
+++ app/dashboard/_components/WelcomeBanner.jsx
@@ -9,7 +9,7 @@ function WelcomeBanner() {
     <div className='p-5 bg-blue-500 w-full text-white rounded-lg flex gap-6 items-center'>
         <Image src={'/laptop.png'} alt='laptop' width={100} height={100}/>
         <div>
-            <h2 classNmae='font-bold text-3xl'>Hello, {user?.fullName}</h2>
+            <h2 className='font-bold text-3xl'>Hello, {user?.fullName}</h2>
             <p>Welcome Back, It's time to get back and start learning new course</p>
         </div>
     </div>
