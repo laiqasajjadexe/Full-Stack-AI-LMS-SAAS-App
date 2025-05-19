9ebfbae8e4c8962c1e56b7c3c77fce33b13595cc
laiqasajjadexe <laiqasajjad56@gmail.com>
2025-05-19 23:45:49 +0500

    fix: format schema.js for improved readability and consistency

--- configs/schema.js
+++ configs/schema.js
@@ -1,9 +1,22 @@
-
+import { json } from 'drizzle-orm/gel-core';
 import {boolean, pgTable, serial, varchar,  } from "drizzle-orm/pg-core";
 
 export const USER_TABLE= pgTable('users', {
-  id: serial().primaryKey(),
-  name:varchar().notNull(),
-  email:varchar().notNull(),
-  isMember:boolean().default(false),
-});
+       id: serial().primaryKey(),
+       name:varchar().notNull(),
+       email:varchar().notNull(),
+       isMember:boolean().default(false),
+}
+);
+export const STUDY_MATERIAL_TABLE= pgTable('studyMaterial',
+    {
+        id: serial().primaryKey(),
+        courseId:varchar().notNull(),
+        courseType: varchar().notNull(),
+        topic: varchar().notNull(),
+        difficultyLevel: varchar().default('Easy'),
+        courseLayout: json(),
+        createdBy: varchar().notNull(),
+        status:varchar().default('Generating'),
+    }
+);
